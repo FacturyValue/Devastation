9c7b1e37f223d6518f3140140b80693ea0a099d0
sirniclolas <1150915116@qq.com>
2019-04-02 01:30:17 +0800

    新增log4j.properties

--- src/main/java/com/data/cboard/Mapper/UserMapper.java
+++ src/main/java/com/data/cboard/Mapper/UserMapper.java
@@ -1,18 +1,44 @@
 package com.data.cboard.Mapper;
 
 import com.data.cboard.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM USER WHERE NAME = #{name}")
+/*    @Select("SELECT * FROM USER WHERE NAME = #{name}")
     User findByName(@Param("name") String name);
 
     @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
     int insert(@Param("name") String name, @Param("age") Integer age);
+
+
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name,jdbcType=VARCHAR}, #{age,jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
+    int insertByUser(User user);*/
+
+    @Select("SELECT * FROM user WHERE name = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+
+    @Update("UPDATE user SET age=#{age} WHERE name=#{name}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
+    @Results({
+            @Result(property = "name", column = "name"),
+            @Result(property = "age", column = "age")
+    })
+    @Select("SELECT name, age FROM user")
+    List<User> findAll();
 
 }
--- src/test/java/com/data/cboard/WebCboardApplicationTests.java
+++ src/test/java/com/data/cboard/WebCboardApplicationTests.java
@@ -12,6 +12,9 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 //@SpringApplicationConfiguration(Application.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -97,7 +100,7 @@ public class WebCboardApplicationTests {
 		Assert.assertEquals(9, userRepository.findAll().size());
 
 	}*/
-	@Autowired
+	/*@Autowired
 	private UserMapper userMapper;
 
 	@Test
@@ -106,6 +109,37 @@ public class WebCboardApplicationTests {
 		userMapper.insert("AAA", 20);
 		User u = userMapper.findByName("AAA");
 		Assert.assertEquals(20, u.getAge().intValue());
+	}*/
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback
+	public void testUserMapper() throws Exception {
+		// insert一条数据，并select出来验证
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+		// update一条数据，并select出来验证
+		u.setAge(30);
+		userMapper.update(u);
+		u = userMapper.findByName("AAA");
+		Assert.assertEquals(30, u.getAge().intValue());
+		// 删除这条数据，并select验证
+		userMapper.delete(u.getId());
+		u = userMapper.findByName("AAA");
+		Assert.assertEquals(null, u);
+	}
+
+	@Test
+	@Rollback
+	public void test2UserMapper() throws Exception {
+		List<User> userList = userMapper.findAll();
+		for(User user : userList) {
+			Assert.assertEquals(null, user.getId());
+			Assert.assertNotEquals(null, user.getName());
+		}
 	}
 
 }
