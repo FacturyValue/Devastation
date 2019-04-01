6c708a5446c6a4c2e791753cb53a2073a81f2dd6
sirniclolas <1150915116@qq.com>
2019-04-02 01:09:29 +0800

    提交Cboard代码

---
+++ src/main/java/com/data/cboard/WebCboardApplication.java
@@ -0,0 +1,13 @@
+package com.data.cboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/cboard/WebCboardApplicationTests.java
@@ -0,0 +1,111 @@
+package com.data.cboard;
+
+import com.data.cboard.Mapper.UserMapper;
+//import com.data.cboard.service.UserRepository;
+import com.data.cboard.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+//@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebCboardApplicationTests {
+
+
+	/*@Autowired
+	private  BlogProperties blogProperties;
+
+	@Test
+	public  void  getHello(){
+		Assert.assertEquals(blogProperties.getName(),"rudy");
+		Assert.assertEquals(blogProperties.getTitle(),"sirniclolas learning springboot");
+	}
+	@Test
+	public void contextLoads() {
+
+	}*/
+	/*@Autowired
+	private UserService userSerivce;
+
+	@Before
+	public void setUp() {
+		// 准备，清空user表
+		userSerivce.deleteAllUsers();
+	}
+
+	@Test
+	public void test() throws Exception {
+		// 插入5个用户
+		userSerivce.create("a", 1);
+		userSerivce.create("b", 2);
+		userSerivce.create("c", 3);
+		userSerivce.create("d", 4);
+		userSerivce.create("e", 5);
+
+		// 查数据库，应该有5个用户
+		Assert.assertEquals(5, userSerivce.getAllUsers().intValue());
+
+		// 删除两个用户
+		userSerivce.deleteByName("a");
+		userSerivce.deleteByName("e");
+
+		// 查数据库，应该有3个用户
+		Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
+
+	}*/
+/*
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void test() throws Exception {
+
+		// 创建10条记录
+		userRepository.save(new User("AAA", 10));
+		userRepository.save(new User("BBB", 20));
+		userRepository.save(new User("CCC", 30));
+		userRepository.save(new User("DDD", 40));
+		userRepository.save(new User("EEE", 50));
+		userRepository.save(new User("FFF", 60));
+		userRepository.save(new User("GGG", 70));
+		userRepository.save(new User("HHH", 80));
+		userRepository.save(new User("III", 90));
+		userRepository.save(new User("JJJ", 100));
+
+		// 测试findAll, 查询所有记录
+		Assert.assertEquals(10, userRepository.findAll().size());
+
+		// 测试findByName, 查询姓名为FFF的User
+		Assert.assertEquals(60, userRepository.findByName("FFF").getAge().longValue());
+
+		// 测试findUser, 查询姓名为FFF的User
+		Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
+
+		// 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
+		Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
+
+		// 测试删除姓名为AAA的User
+		userRepository.delete(userRepository.findByName("AAA"));
+
+		// 测试findAll, 查询所有记录, 验证上面的删除是否成功
+		Assert.assertEquals(9, userRepository.findAll().size());
+
+	}*/
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback
+	public void findByName() throws Exception {
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+	}
+
+}
